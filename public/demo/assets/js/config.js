1b9ea5bc8e253ef7de60af16ec521076efb129f8
naghamalaatki <naghamalaatki@gmail.com>
2026-03-07 14:43:17 +0100

    Config

--- public/demo/assets/js/config.js
+++ public/demo/assets/js/config.js
@@ -2,14 +2,15 @@ export const config = {
     apiBase: "/api",
     debug: true,
     pusher: {
-        // key: "e5522d137bf0f04ea59c",
-        // cluster: "eu",
+         key: "e5522d137bf0f04ea59c",
+         cluster: "eu",
         authEndpoint: "/broadcasting/auth",
-        key: "886ffa931b99f50946be",
-        cluster: "mt1",
+      //  key: "886ffa931b99f50946be",
+       // cluster: "mt1",
     },
     stripe: {
         publishableKey:
-            "pk_test_51SybpZDoWOc1Wt5yaQblcdEjWH6sLOjpiyARQp3oK1arufahBDCKqzOjFzlXihEJKsxqfijxDnunYdUqYGIE7f5700f788ZRQI",
+       "pk_test_51T1TZ6RjfPr6Wc8zjkQ3LnBccXEkPS6H4lol1SzbIRuAvhRTu8m8j2gLauFWY4eBzKgQGNMi7pif9cdreSp1Zuzi00A6FhL1bn",
+           
     },
 };
